0d51209b573a8b2bd58835a0c475009c4b7603e8
Henrique Antônio Guanais Corneau <109967494+henriqueguanais@users.noreply.github.com>
2024-03-24 14:25:36 -0300

    Create questao_02.c

---
+++ questoes/questao_02.c
@@ -0,0 +1,9 @@
+/*
+Questão 2:
+- 4094, pois "p" imprime o endereço de i
+- 4096, pois "p+1" imprime a posição de p (4094) deslocado de 1, que equivale a 2 bytes, 4094 + 2 = 4096
+- 7, pois 5 + 2 = 7
+- 5, pois **&p se refere ao conteudo de i
+- 15, pois 3*5 = 15
+- 9, pois 5 + 4 = 9
+*/
