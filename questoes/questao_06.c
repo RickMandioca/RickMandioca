02434d0db08f531ca575c02d5752a08a71fffb85
Henrique Antônio Guanais Corneau <109967494+henriqueguanais@users.noreply.github.com>
2024-03-30 10:11:03 -0300

    Add files via upload

---
+++ questoes/questao_06.c
@@ -0,0 +1,10 @@
+/*
+Questão 6:
+
+A questão pergunta qual das 3 expressões referencie o terceiro valor do vetor, ou seja, qual aponta para o endereço de memória do terceiro valor
+do vetor.
+
+As duas primeiras, apenas mostram o conteúdo do vetor deslocado, pulo + 4 referencia a 5 posição do vetor, enquanto a última que referencia o
+terceiro valor do vetor.
+
+*/
