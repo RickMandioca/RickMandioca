2bcffd870e2b4be48a15561e71109f3ea0a8c5b1
Henrique Antônio Guanais Corneau <109967494+henriqueguanais@users.noreply.github.com>
2024-03-30 11:22:57 -0300

    Add files via upload

---
+++ questoes/questao_08.c
@@ -0,0 +1,7 @@
+/*
+Questão 8:
+
+O primeiro programa mostra o conteúdo do array vet, mostrando os valores de todos os seus índices;
+O segundo programa mostra o endereço de memória ocupado por cada valor do array vet.
+
+*/
