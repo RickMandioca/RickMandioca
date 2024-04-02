48690ff33080eb4eaeb2f1b67c9adee07bd01e9e
Henrique Antônio Guanais Corneau <109967494+henriqueguanais@users.noreply.github.com>
2024-04-02 20:18:52 -0300

    Add files via upload

---
+++ questoes/questao_23.c
@@ -0,0 +1,11 @@
+/*
+Questão 23:
+
+I - Verdadeiro, a primeira função irá retornar o indice quando 15 == vetor[i], que é 7. Ja na segunda função, a função irá também fazer a mesma coisa
+como os parêmetros são para achar o índice que tem o valor 15, então irá retornar 7.
+II - Falso, pois no pior caso, com um vetor muito grande, a primeira função irá verificar cada valor do vetor, já a segunda vai eliminar metade
+do vetor e fazer as verificações.
+III - Falso, a função que implementa uma estratégia iterativa é a função 1.
+
+Resposta: Somente I.
+*/
